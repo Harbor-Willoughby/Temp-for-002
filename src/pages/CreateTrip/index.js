89d6de14319a2ff3baf00d6f741ee43bc44d34af
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-10-22 07:30:28 +0900

    Fix css

--- src/pages/CreateTrip/index.js
+++ src/pages/CreateTrip/index.js
@@ -77,56 +77,58 @@ export default class CreateTripPage extends React.Component {
     const endDateString = endDate && endDate.format('YYYY-MM-DD');
 
     return (
-      <div
-        className="create-trip__wrapper"
-        style={{
-          backgroundImage: `url(${bgImage})`,
-        }}
-      >
-        <div className="create-trip--dimmer">
-          <header className="create-trip--nav">
-            <div className="create-trip__logo">
-              Scwrap
-            </div>
-            <div>
-              <img src={accountIcon} />
-            </div>
-          </header>
-          <form className="new-trip" onSubmit={this.handleSubmit.bind(this)}>
-          <div className="create-trip--title-wrapper">
-            <div className="create-trip--title">
-              Scwrap에서 여행을 SCRAP하세요!
-            </div>
-          </div>
-          <div className="create-trip--form-wrapper">
-            <div className="create-trip--input-wrapper">
-              <input
-                className="create-trip--text-input"
-                value={ name } type="text" onChange={ (e) => this.handleChange('name', e.target.value) }
-              ref="textInput" />
-            </div>
-            <div className="create-trip--input-wrapper">
-              <div className="create-trip--date-input">
-                {startDateString} ~ {endDateString}
-                <img className="create-trip--date-img" alt="" src={ calendarIcon } onClick={ _.partial(this.handleChange, 'showDate', !showDate) } ref="dateInput" />
+      <div style={{ width: '100%', height: '100vh', }}>
+        <div
+          className="create-trip__wrapper"
+          style={{
+            backgroundImage: `url(${bgImage})`,
+          }}
+        >
+          <div className="create-trip--dimmer">
+            <header className="create-trip--nav">
+              <div className="create-trip__logo">
+                Scwrap
               </div>
-              { showDate && <div className="create-trip--input-calendar">
-                <FromToDate
+              <div>
+                <img src={accountIcon} />
+              </div>
+            </header>
+          <form className="new-trip" onSubmit={this.handleSubmit.bind(this)}>
+            <div className="create-trip--title-wrapper">
+              <div className="create-trip--title">
+                Scwrap에서 여행을 SCRAP하세요!
+              </div>
+            </div>
+            <div className="create-trip--form-wrapper">
+              <div className="create-trip--input-wrapper">
+                <input
+                  className="create-trip--text-input"
+                  value={ name } type="text" onChange={ (e) => this.handleChange('name', e.target.value) }
+                  ref="textInput" />
+              </div>
+              <div className="create-trip--input-wrapper">
+                <div className="create-trip--date-input">
+                  {startDateString} ~ {endDateString}
+                  <img className="create-trip--date-img" alt="" src={ calendarIcon } onClick={ _.partial(this.handleChange, 'showDate', !showDate) } ref="dateInput" />
+                </div>
+                { showDate && <div className="create-trip--input-calendar">
+                  <FromToDate
                     startDate={ startDate }
                     endDate={ endDate }
                     handleStartDateChange={ _.partial(this.handleChange, 'startDate', _) }
                     handleEndDateChange={ _.partial(this.handleChange, 'endDate', _) }
                     onBlur={ _.partial(this.handleChange, 'showDate', !showDate) }
-                /></div> }
-            </div>
-            <div>
+                  /></div> }
+              </div>
               <div>
+                <div>
                   <button type="submit" className="create-trip__button">NEXT</button>
+                </div>
               </div>
             </div>
-          </div>
           </form>
         </div>
+      </div>
       </div>
     );
   }
