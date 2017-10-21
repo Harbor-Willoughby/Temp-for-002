56bf589aef5dabec031aa9372156fe817ce46c5f
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-10-21 19:34:45 +0900

    Redux thunk 를 Redux saga로 교체했습니다

--- src/actions.js
+++ src/actions.js
@@ -4,31 +4,9 @@ const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 
-export const loginGoogleUser = () => {
-  return (dispatch) => {
-    // 로그인 요청중 Action
-    dispatch({
-      type: 'LOGIN_USER_REQUEST',
-    })
-
-    auth.signInWithPopup(googleProvider)
-      .then((user) => {
-        dispatch({
-          type: 'LOGIN_USER_SUCCESS',
-          payload: {
-            name: user.additionalUserInfo.profile.name,
-            profileImageUrl: user.additionalUserInfo.profile.picture,
-            email: user.additionalUserInfo.profile.email,
-          }
-        })
-      })
-      .catch((error) => {
-        dispatch({
-          type: 'LOGIN_USER_FAILED',
-        })
-      })
-  }
-}
+export const loginGoogleUser = () => ({
+  type: 'LOGIN_USER_REQUEST',
+})
 
 export const logout = () => {
   return (dispatch) => {
---
+++ src/sagas.js
@@ -0,0 +1,21 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import firebase from './firebase';
+
+const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+
+function* loginGoogleUser() {
+  try {
+    const user = yield call(() => auth.signInWithPopup(googleProvider));
+    yield put({type: "LOGIN_USER_SUCCESS", payload: user});
+  } catch (e) {
+    yield put({type: "LOGIN_USER_FAILED", payload: e.message});
+  }
+}
+
+function* mySaga() {
+  yield takeEvery("LOGIN_USER_REQUEST", loginGoogleUser);
+}
+
+export default mySaga;
