92b7b370020c7503702a82ae52a10408a079e0d0
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-10-22 08:51:03 +0900

    Add base for AddEventModal

--- src/pages/register-trip/register-trip.js
+++ src/pages/register-trip/register-trip.js
@@ -6,8 +6,20 @@ import map from 'lodash/map';
 import bgImage from './bg02.jpg';
 import logoImage from './logo.png';
 import popbuttonImage from './pop-button.png';
+import AddEventModal from './AddEventModal';
+
 
 export default class RegisterTrip extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalDisplayed: true,
+    }
+  }
+
+  toggleModalOpen = () => this.setState({ isModalDisplayed: !this.state.isModalDisplayed })
+
   handleSubmit(event) {
     var pair_key = this.props.location.state.data;
 
@@ -33,16 +45,27 @@ export default class RegisterTrip extends React.Component {
   render() {
     document.body.style.backgroundImage = `url(${bgImage})`; 
     return (
-      <div id="page-wrapper">
-      <img src={logoImage} className="logo" />
-      <div>
-        <h2>당신의 여행 첫째날을 SCRAP 하세요!</h2>
-        <h3>사진, 링크, 메모 등 자유롭게 이용하실 수 있습니다.</h3>
-      </div>
-      <div>
-        <img src={popbuttonImage} className="pop-button" />
-      </div>
-      {/*
+      <div
+        id="page-wrapper"
+        style={{ position: 'relative' }}
+      >
+        {this.state.isModalDisplayed && <AddEventModal closeModal={this.toggleModalOpen} />}
+        <div>
+          <img src={logoImage} className="logo" />
+          <div>
+            <h2>당신의 여행 첫째날을 SCRAP 하세요!</h2>
+            <h3>사진, 링크, 메모 등 자유롭게 이용하실 수 있습니다.</h3>
+          </div>
+          <div
+            onClick={this.toggleModalOpen}
+          >
+            <img
+              src={popbuttonImage}
+              className="pop-button"
+            />
+          </div>
+        </div>
+        {/*
       <form className="new-event" onSubmit={this.handleSubmit.bind(this)} >
 
         <input
@@ -57,7 +80,7 @@ export default class RegisterTrip extends React.Component {
 
       </form>
       */}
-      </div>
+    </div>
     );
   }
 }
