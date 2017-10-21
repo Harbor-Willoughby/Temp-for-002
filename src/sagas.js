72f9a3e329292c481e05506d9a69d4168343524f
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-10-21 21:31:42 +0900

    새로고침을 하면 인증 상태가 초기화되던 문제를 개선했습니다

--- src/sagas.js
+++ src/sagas.js
@@ -7,15 +7,22 @@ const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 function* loginGoogleUser() {
   try {
-    const user = yield call(() => auth.signInWithPopup(googleProvider));
-    yield put({type: "LOGIN_USER_SUCCESS", payload: user});
+    const user = yield auth.signInWithPopup(googleProvider);
+    // 용석: Firebase Auth의 onAuthStateChanged method로 로그인 변경 상태 관리하는 것으로 변경
+    // yield put({type: "LOGIN_USER_SUCCESS", payload: user});
   } catch (e) {
+    // 로그인 에러가 발생한 경우에만 핸들링
     yield put({type: "LOGIN_USER_FAILED", payload: e.message});
   }
 }
 
+function logoutUser() {
+  return auth.signOut();
+}
+
 function* mySaga() {
   yield takeEvery("LOGIN_USER_REQUEST", loginGoogleUser);
+  yield takeEvery("LOGOUT_USER", logoutUser);
 }
 
 export default mySaga;
