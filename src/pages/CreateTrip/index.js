5bc3bb0c364f82f77a99cfe602fdf85e69e9b1b1
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-10-22 01:17:55 +0900

    인라인 스타일을 별도 scc 파일로 옮겼습니다

--- src/pages/CreateTrip/index.js
+++ src/pages/CreateTrip/index.js
@@ -13,20 +13,8 @@ export default class CreateTripPage extends React.Component {
           backgroundImage: `url(${bgImage})`,
         }}
       >
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'rgba(0, 0, 0, 0.4)'
-          }}
-        >
-          <header
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              padding: '17px 25px',
-            }}
-          >
+        <div className="create-trip--dimmer">
+          <header className="create-trip--nav">
             <div className="create-trip__logo">
               Scwrap
             </div>
@@ -34,75 +22,20 @@ export default class CreateTripPage extends React.Component {
               <img src={accountIcon} />
             </div>
           </header>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <div
-              style={{
-                fontSize: '80px',
-                color: 'white',
-                marginTop: '270px',
-              }}
-            >
+          <div className="create-trip--title-wrapper">
+            <div className="create-trip--title">
               Scwrap에서 여행을 SCRAP하세요!
             </div>
           </div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: '120px',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                width: '668px',
-              }}
-            >
+          <div className="create-trip--form-wrapper">
+            <div className="create-trip--input-wrapper">
               <input
-                style={{
-                  backgroundColor: 'transparent',
-                  border: 'none',
-                  outline: 'none',
-                  borderBottom: '3px solid #8e88ff',
-                  color: 'white',
-                  textAlign: 'center',
-                  fontSize: '30px',
-                  padding: '10px',
-                  height: '50px',
-                  width: '100%',
-                }}
+                className="create-trip--text-input"
                 value="런던" type="text" 
               />
             </div>
-            <div
-              style={{
-                display: 'flex',
-                width: '668px',
-              }}
-            >
-              <div
-                style={{
-                  marginTop: '20px',
-                  backgroundColor: 'transparent',
-                  border: 'none',
-                  outline: 'none',
-                  borderBottom: '3px solid #8e88ff',
-                  color: 'white',
-                  textAlign: 'center',
-                  fontSize: '30px',
-                  padding: '10px',
-                  height: '50px',
-                  width: '100%',
-                }}
-              >
+            <div className="create-trip--input-wrapper">
+              <div className="create-trip--date-input">
                 10월 21일 ~ 10월 31일
               </div>
             </div>
